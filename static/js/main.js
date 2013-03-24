e01903b274e6d916e67b4b08a4ead07daba8c5d5
lkvsky <kyle@lucovsky.com>
2013-03-24 12:21:41 -0700

    set up require js

--- static/js/main.js
+++ static/js/main.js
@@ -1,58 +1,12 @@
-var Static = (function() {
-  function PaintCanvas(height, width, ctx) {
-    var that = this;
+require.config({
 
-
-    that.initialize = function() {
-      ctx.canvas.height = height;
-      ctx.canvas.width = width;
-      setTimeout(that.fillCanvas, 255);
-      that.canvasHover();
-    };
-
-    that.fillCanvas = function() {
-      var imageData = ctx.getImageData(0, 0, width, height);
-
-      for (var i=0; i<width; i++) {
-        for (var j=0; j<height; j++) {
-          var shade = Math.floor(Math.random() * 255);
-
-          index = (i + j * width) * 4;
-          imageData.data[index] = shade;
-          imageData.data[index+1] = shade;
-          imageData.data[index+2] = shade;
-          imageData.data[index+3] = 255;
-        }
-      }
-
-      ctx.putImageData(imageData, 0, 0);
-      that.drawText();
-    };
-
-    that.drawText = function() {
-      ctx.beginPath();
-      ctx.fillStyle = "white";
-      ctx.font = "bold 35pt Arvo";
-      ctx.fillText("{kwl}", 12, 100);
-    };
-
-    that.canvasHover = function() {
-      $(ctx.canvas).hover(function() {
-        that.interval = setInterval(that.fillCanvas, 100/33);
-      }, function() {
-        clearInterval(that.interval);
-      });
-    };
+  paths: {
+    jquery: 'lib/jquery-1-8-0-min'
   }
 
-  return {
-    PaintCanvas: PaintCanvas
-  };
-})();
+});
 
-(function() {
-  var mainCanvas = document.getElementsByClassName("logo")[0];
-  var ctx = mainCanvas.getContext("2d");
-  var paintCanvas = new Static.PaintCanvas(160, 160, ctx);
-  paintCanvas.initialize();
-})();
+require(['modules/canvas_view'], function(CanvasView){
+  var logo = new CanvasView();
+  logo.initialize();
+});
---
+++ static/js/modules/globals.js
@@ -0,0 +1,5 @@
+define({
+  ctx: document.getElementsByTagName('canvas')[0].getContext("2d"),
+  height: 160,
+  width: 160
+});
