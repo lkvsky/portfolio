3e63543447a53a418224086bef86605b43975630
lkvsky <kyle@lucovsky.com>
2013-04-10 09:29:44 -0700

    configured grunt to serve minified js in production

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,32 +5,22 @@ module.exports = function(grunt) {
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
  
-    concat: {
-      'myproject': {
-        src: [ 'static/js/lib/handlbars.js',
-               'static/js/lib/jquery-1-8-0-min.js',
-               'static/js/lib/require.js',
-               'static/js/lib/text.js',
-               'static/js/main.js',
-               'static/js/modules/app.js',
-               'static/js/modules/background_view.js',
-               'static/js/modules/bubble.js',
-               'static/js/modules/logo_view.js' ],
-        dest: 'static/build/all.js'
-      }
-    },
- 
-    uglify: {
-      'myproject': {
-        src: 'static/build/all.js',
-        dest: 'static/build/all.min.js'
-      }
-    },
- 
     mincss: {
       'myproject': {
         files: {
           'static/build/main.min.css': [ 'static/css/main.css']
+        }
+      }
+    },
+
+    requirejs: {
+      'myproject': {
+        options: {
+          name: 'main',
+          baseUrl: 'static/js',
+          mainConfigFile: 'static/js/main.js',
+          optimize: "uglify",
+          out: 'static/js/optimized-main.js'
         }
       }
     }
@@ -39,7 +29,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-mincss');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
  
   // Default task.
-  grunt.registerTask('default', ['concat:myproject', 'uglify:myproject', 'mincss:myproject']);
+  grunt.registerTask('default', ['mincss:myproject', 'requirejs:myproject']);
 };
